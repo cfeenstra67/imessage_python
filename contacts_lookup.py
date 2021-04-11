4e63371da6255058fb0005e3bfd849f3e3a5eb74
cfeenstra67 <cameron.l.feenstra@gmail.com>
2021-04-10 22:29:37 -0400

    missing files

--- contacts_lookup.py
+++ contacts_lookup.py
@@ -9,13 +9,13 @@ def _db_path():
 
 def number_format_strings(country_code=True):
 	codes = [
-		'({ac}) {f3}-{l4}',
+		'({ac})%{f3}-{l4}',
 		'{ac}{f3}{l4}'
 	]
 	if country_code:
 		codes.extend([
 				'{cc}{ac}{f3}{l4}',
-				'{cc} ({ac}) {f3}-{l4}',
+				'{cc}%({ac})%{f3}-{l4}',
 			])
 	return codes
 
@@ -24,7 +24,7 @@ def possible_formats(phone_number):
 	parts = {'cc': ''}
 	if len(phone_number) > 10:
 		parts['cc'] = phone_number[:-10]
-		phone_number = phone_number[2:]
+		phone_number = phone_number[-10:]
 	parts['ac'] = phone_number[:3]
 	parts['f3'] = phone_number[3:6]
 	parts['l4'] = phone_number[6:]
@@ -35,12 +35,25 @@ with sqlite3.connect(_db_path()) as conn:
 	def lookup_id(phone_number):
 		results = []
 		for formatted_num in possible_formats(phone_number):
-			curs.execute('SELECT ZOWNER FROM ZABCDPHONENUMBER WHERE ZFULLNUMBER=?', (formatted_num,))
+			curs.execute('SELECT ZOWNER FROM ZABCDPHONENUMBER WHERE ZFULLNUMBER LIKE ?', (formatted_num,))
 			results.extend(curs.fetchall())
 		return [result for (result,) in results][0] if len(results) > 0 else None
 	def lookup_name_id(unique_id):
 		curs.execute('SELECT ZFIRSTNAME, ZLASTNAME, ZNICKNAME FROM ZABCDRECORD WHERE Z_PK=?', (unique_id,))
-		return curs.fetchall()[0] if len(results) > 0 else None
+		results = curs.fetchall()
+		return results[0] if len(results) > 0 else None
 	def lookup_name(phone_number):
-		first, last, nickname = lookup_name_id(lookup_id(phone_number))
+		first, last, nickname = lookup_name_id(lookup_id(phone_number) or '') or (None, None, None)
 		return {'first': first, 'last': last, 'nickname': nickname}
+	def lookup_phone_number(name):
+		try:
+			first, last = name.split(' ')
+			curs.execute('SELECT Z_PK FROM ZABCDRECORD WHERE ZFIRSTNAME LIKE ? AND ZLASTNAME LIKE ?', (first, last))
+		except ValueError:
+			curs.execute('SELECT Z_PK FROM ZABCDRECORD WHERE ZFIRSTNAME LIKE ? OR ZLASTNAME LIKE ?', (name, name))
+		unique_id = curs.fetchone()
+		if not unique_id: return None
+		curs.execute('SELECT ZFULLNUMBER FROM ZABCDPHONENUMBER WHERE ZOWNER=?', unique_id)
+		(full_num,) = curs.fetchone()
+		return full_num.translate({ord(c): None for c in '() -' + chr(160)})
+
--- imessage.py
+++ imessage.py
@@ -2,11 +2,24 @@
 
 import sqlite3
 import os
+from contacts_lookup import lookup_name
+from pprint import pprint
+from datetime import datetime
 
 with sqlite3.connect(os.path.expanduser('~/Library/Messages/chat.db')) as conn:
-	cursor = conn.cursor()
-
+	curs = conn.cursor()
+	def _get_phonenum(handle_id):
+		curs.execute('SELECT id FROM handle WHERE ROWID=?', (handle_id,))
+		results = curs.fetchall()
+		return [result for (result,) in results][0] if len(results) > 0 else None
+	def get_unread():
+		curs.execute('SELECT text, handle_id, date FROM message WHERE (is_read=0 AND is_from_me=0 AND is_empty=0 AND is_delivered=1)')
+		default_val = 'UNKNOWN'
+		for text, handle_id, date in curs.fetchall():
+			phone_number = _get_phonenum(handle_id) or default_val
+			print(phone_number)
+			name = lookup_name(phone_number) or default_val
+			yield {'name': name, 'phone_number': phone_number, 'text': text}
 
 	if __name__ == '__main__':
-		cursor.execute('SELECT * FROM message')
-		print(cursor.fetchall()[:10])
+		pprint([*get_unread()])
